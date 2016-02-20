6ecee70583a93d2eaa01152ca706d154b1ae4d46
Ben Attenborough <battenborough@gmail.com>
2016-02-20 22:43:57 +0000

    Created a search file

--- js/search.js
+++ js/search.js
@@ -2,11 +2,14 @@ console.log('search.js working');
 
 // Plan of action
 //
+// Create an object holding all the pictures and meta data (title, alt text, urls)
 // When character typed into search box search image captions for search term
 // return all images that match search term
 
 var searchTerm;
 
 $(".search__form__input").keyup( function() {
-	console.log("Key pressed");
+	searchTerm = $(".search__form__input").val()
+	console.log("Key pressed: ");
+	console.log(searchTerm);
 });
