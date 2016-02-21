cb9264833cd4b96985045c41b81a0c19e9a38eb3
Ben Attenborough <battenborough@gmail.com>
2016-02-21 10:57:49 +0000

    Tidying up

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-console.log('main.js working');
-
-// Plan of action
-//
-// 0. Setup
-//  0.1 Create an overlay for the lightbox
-//  0.2 Setup variable for image
-//  0.3 Setup variable for caption
-// 1. Detect when user clicks on an image 
-// 	1.1 Select the image
-//  1.2 Add a click event
-// 2. Prevent default action and add new functionality
-//  2.1 Prevent default
-//	2.2 Add new functionality
-// 3. Create an overlay for image and show image on it
-//	3.1 Create overlay
-//  3.2 Select the overlay
-//	3.3 Display the overlay
-//  3.4 Select the image
-//  3.5 Display the image
-// 4. Show caption below the image
-//  4.1 Select the alt text
-//	4.2 Display the alt text
-// 5. Show previous and next buttons
-//  5.1 Create the buttons
-//  5.2 Add click event to the buttons
-//  5.3 When button clicked display previous or next image
-//   5.3.1 Get the next/previous image using jquery
-// 8. Close image overlay on click
-
 var $overlay = $("<div id='overlay'></div>");
 var $previousBtn = $("<div class='col-prev clearfix'><a href='#'><img src='img/previousBtn.png' class='nav-btn'></a></div>");
 var $contentDiv = $("<div class='col-main clearfix'></div>");
@@ -55,8 +25,6 @@ function changeImage(direction) {
 	$replacementImage = $imageData.children("a").attr("href");
 	$replacementAltText = $imageData.children("a").children("img").attr("alt");
 	$image.attr("src", $replacementImage);
-	// $image.attr("alt", $replacementAltText);
-	console.log($replacementAltText);
 	$caption.html($replacementAltText);
 };
 
@@ -67,24 +35,14 @@ $overlay.append($contentDiv);
 $overlay.append($nextBtn);
 $("body").prepend($overlay);
 
-// 1. Detect when user clicks on an image
-// 	1.1 Select the image
 
-//  1.2 Add a click event
 $(".pictures a").click( function(){
-	// 2. Prevent default action and add new functionality
-	//  2.1 Prevent default
-	//	2.2 Add new functionality
 	event.preventDefault();
 	var imageLocation = $(this).attr("href");
-	
 	$image.attr("src", imageLocation);
-
 	$("#overlay").show();
-
 	var captionText = $(this).children("img").attr("alt");
 	$caption.text(captionText);
-
 	//Get image index (location of image in list) for use in prev / next buttons
 	imageIndex = $(this).parent().index();
 });
@@ -92,8 +50,6 @@ $(".pictures a").click( function(){
 $("#overlay").click( function(){
 	$(this).hide();
 });
-
-// $("#overlay").event.stopPropagation()
 
 $(".col-next a").click( function(event){
 	event.stopPropagation();
